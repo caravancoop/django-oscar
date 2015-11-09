ed11ea203731776608eb1ed8667a3d654a1941d3
Simon Chenard <chenard.simon@gmail.com>
2015-11-09 16:53:47 -0500

    disabled the review creation - in the meantime before we can upgrade the whole app

--- oscar/apps/catalogue/reviews/app.py
+++ oscar/apps/catalogue/reviews/app.py
@@ -7,14 +7,14 @@ from oscar.apps.catalogue.reviews import views
 class ProductReviewsApplication(Application):
     name = None
     detail_view = views.ProductReviewDetail
-    create_view = views.CreateProductReview
+    #create_view = views.CreateProductReview
     list_view = views.ProductReviewList
 
     def get_urls(self):
         urlpatterns = patterns('',
             url(r'^(?P<pk>\d+)/$', self.detail_view.as_view(),
                 name='reviews-detail'),
-            url(r'^add/$', self.create_view.as_view(), name='reviews-add'),
+            #url(r'^add/$', self.create_view.as_view(), name='reviews-add'),
             url(r'^$', self.list_view.as_view(), name='reviews-list'),
         )
         return urlpatterns
